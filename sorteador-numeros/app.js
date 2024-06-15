9acc46277476b1d85b839f539e7e3aa86349c58c
Heloisa Mayerle Lamego <110624387+HeloisaMayerle@users.noreply.github.com>
2024-06-15 17:50:11 -0300

    Update 15.06 app.js

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,9 +1,15 @@
-function sortear() {
-    let quantidade = document.getElementById('quantidade').value;
-    let de = gameClicado.getElementById('de').value;
-    let ate = gameClicado.getElementById('ate').value;
-
-    alert(`Quantidade de números: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Até: ${ate}`);
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    
+    let sorteados = [];
+    let numero;
+    
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+    
+    alert(sorteados);
 }
